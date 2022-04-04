09681f68aaf9a456b0ca178402a82d68dd9025cb
Tadeas Kriz <tadeas@touchlab.co>
2022-04-04 18:48:03 -0400

    Set correct head branch.

--- xcode-kotlin.rb
+++ xcode-kotlin.rb
@@ -2,7 +2,7 @@ class XcodeKotlin < Formula
   desc "Kotlin Native Xcode Plugin"
   homepage "https://github.com/touchlab/xcode-kotlin"
   url "https://github.com/touchlab/xcode-kotlin.git", :tag => "0.2.2"
-  head "https://github.com/touchlab/xcode-kotlin.git"
+  head "https://github.com/touchlab/xcode-kotlin.git", branch: "main"
   license "Apache-2.0"
 
   def install
