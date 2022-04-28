419b79b5505bde6a5f41201435fb367552a4aa91
Tadeas Kriz <tadeas@touchlab.co>
2022-04-28 17:12:52 -0400

    Update xcode-kotlin formula with preparePlugin gradle task.

--- xcode-kotlin.rb
+++ xcode-kotlin.rb
@@ -1,46 +1,46 @@
 class XcodeKotlin < Formula
   desc "Kotlin Native Xcode Plugin"
   homepage "https://github.com/touchlab/xcode-kotlin"
-  url "https://github.com/touchlab/xcode-kotlin.git", :tag => "v0.2.2"
-  head "https://github.com/touchlab/xcode-kotlin.git", branch: "cli"
+  url "https://github.com/touchlab/xcode-kotlin.git", tag: "v0.2.2"
   license "Apache-2.0"
-  depends_on :xcode
+  head "https://github.com/touchlab/xcode-kotlin.git", branch: "cli"
   depends_on "java" => :build
+  depends_on :xcode
 
   def install
     ohai "Deciding which architecture to build"
     arch = `uname -m`.strip
 
     case arch
-    when 'arm64'
-      buildTask = 'linkReleaseExecutableMacosArm64'
-      buildDir = 'macosArm64'
-    when 'x86_64'
-      buildTask = 'linkReleaseExecutableMacosX64'
-      buildDir = 'macosX64'
+    when "arm64"
+      build_task = "linkReleaseExecutableMacosArm64"
+      build_dir = "macosArm64"
+    when "x86_64"
+      build_task = "linkReleaseExecutableMacosX64"
+      build_dir = "macosX64"
     else
       odie "Unsupported macOS architecture #{arch}."
     end
     ohai "Building executable for #{arch}"
-    buildStatus = runShell "./gradlew --no-daemon #{buildTask}"
-    odie "Build FAILED" unless buildStatus.success?
-    bin.install "build/bin/#{buildDir}/releaseExecutable/xcode-kotlin.kexe" => 'xcode-kotlin'
-    share.install Dir["data/*"]
+    build_status = run_shell "./gradlew --no-daemon #{build_task} preparePlugin"
+    odie "Build FAILED" unless build_status.success?
+    bin.install "build/bin/#{build_dir}/releaseExecutable/xcode-kotlin.kexe" => "xcode-kotlin"
+    share.install Dir["build/share/*"]
   end
 
   test do
-    runShell "xcode-kotlin info"
+    run_shell "xcode-kotlin info"
   end
 
-  def runShell(command)
+  def run_shell(command)
     Open3.popen3(command) do |stdin, stdout, stderr, thread|
       Thread.new do
-        until (line = stdout.gets).nil? do
+        until (line = stdout.gets).nil?
           $stdout.puts(line)
         end
       end
       Thread.new do
-        until (line = stderr.gets).nil? do
+        until (line = stderr.gets).nil?
           $stderr.puts(line)
         end
       end
